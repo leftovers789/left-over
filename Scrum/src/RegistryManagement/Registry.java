032a7b42a5857fc74467949d6cd1bcd50c4daf37
Jechris Soldevilla <>
2015-03-01 14:07:27 -0800

    Working Test > ScrumProject, Working all Frames

--- Scrum/src/RegistryManagement/Registry.java
+++ Scrum/src/RegistryManagement/Registry.java
@@ -9,7 +9,6 @@ public class Registry extends Observable {
 
     private List<BusTravelLog> busTravelLogs = new ArrayList<>();
     private List<DutyLog> dutyLogs = new ArrayList<>();
-    private List<RepairLog> repairLogs = new ArrayList<>();
     private List<SalesLog> salesLogs = new ArrayList<>();
     private List<Trip> trips = new ArrayList<>();
     private static Registry registry = null;
@@ -38,14 +37,6 @@ public class Registry extends Observable {
 
     public void addDutyLog(DutyLog dutyLogs) {
         this.dutyLogs.add(dutyLogs);
-    }
-
-    public List<RepairLog> getRepairLogs() {
-        return repairLogs;
-    }
-
-    public void addRepairLog(RepairLog repairLog) {
-        this.repairLogs.add(repairLog);
     }
 
     public List<SalesLog> getSalesLogs() {
